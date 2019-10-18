5181e2e172608834cb244cc8a74f20a41995b5fa
KrisiKostadinov <krisi.199898@gmail.com>
2019-10-18 12:57:01 +0300

    Admin panel with Users, Posts, Categories.

--- src/BlogApp/BlogApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/BlogApp/BlogApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using BlogApp.DataModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -19,14 +20,14 @@ namespace BlogApp.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<BlogUser> _signInManager;
+        private readonly UserManager<BlogUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
         public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<BlogUser> userManager,
+            SignInManager<BlogUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
@@ -74,7 +75,7 @@ namespace BlogApp.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new BlogUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
---
+++ src/BlogApp/BlogApp/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private const string AdminLayout = "AdminLayout";
+
+        public IActionResult Index()
+        {
+            return View(nameof(Index), nameof(AdminLayout));
+        }
+
+        public IActionResult AllUsers()
+        {
+            return View(nameof(AllUsers), nameof(AdminLayout));
+        }
+
+        public IActionResult AllPosts()
+        {
+            return View(nameof(AllPosts), nameof(AdminLayout));
+        }
+
+        public IActionResult AllCategories()
+        {
+            return View(nameof(AllCategories), nameof(AdminLayout));
+        }
+    }
+}
